a32819fa07b1b70a0261b8619ae444161acbb728
peyberne <mathieu.peybernes@epfl.ch>
2026-09-12 18:07:41 +0200

    env: switch Pitagora runtime to PETSc module (nvhpc 25.11, petsc4py 3.22.0)

--- env_pitagora_python-petsc.sh
+++ env_pitagora_python-petsc.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 # Pitagora runtime environment for petsc-python-miniapp
-# Does NOT source env_pitagora_build.sh (module purge not needed at runtime)
+module purge
+module load nvhpc/25.11
+module load petsc/3.22.0--hpcx-mpi--2.25.1--nvhpc--25.11-mumps
 
-export PETSC_DIR=/pitagora/home/userexternal/mpeybern/petsc-install-python
 export PETSC_ARCH=
-export PYTHONPATH=$PETSC_DIR/lib:$PYTHONPATH
-
 source /pitagora/home/userexternal/mpeybern/petsc-python-miniapp/myenv/bin/activate
 
-# CUDA 12.9 (nvhpc bundled) for runtime
-NVHPC_BASE=${NVHPC_HOME:-/pitagora/prod/spack/6.1/install/0.22/linux-rhel9-zen4/gcc-11.4.1/nvhpc-25.11-ntshdsgl52b6ckb6iu7xazd6uvqi3wqi/Linux_x86_64/25.11}
-CUDA126_LIB=/pitagora/prod/spack/6.1/install/0.22/linux-rhel9-zen4/gcc-11.4.1/cuda-12.6.0-mpuxcbikjk6ksvpkfapavgd7s27by2ac/lib64
+NVHPC_BASE=/pitagora/prod/spack/6.1/install/0.22/linux-rhel9-zen4/gcc-11.4.1/nvhpc-25.11-ntshdsgl52b6ckb6iu7xazd6uvqi3wqi/Linux_x86_64/25.11
 
 # Fix nvhpc OpenMPI hardcoded /proj/nv path
 OMPI_ROOT=$NVHPC_BASE/comm_libs/13.0/hpcx/hpcx-2.25.1/ompi
 export OPAL_PREFIX=$OMPI_ROOT
 
-export LD_LIBRARY_PATH=$PETSC_DIR/lib:$OMPI_ROOT/lib:$NVHPC_BASE/cuda/12.9/lib64:$NVHPC_BASE/math_libs/12.9/targets/x86_64-linux/lib:${CUDA126_LIB}:${LD_LIBRARY_PATH:-}
+export LD_LIBRARY_PATH=$OMPI_ROOT/lib:${LD_LIBRARY_PATH:-}
 
 # GPU Direct
 export MPICH_GPU_SUPPORT_ENABLED=1
